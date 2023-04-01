e6e7412777c5467e11bf15b5444642e238ba46eb
Ashish Dhakal <dhakalashish18@gmail.com>
2023-04-01 11:01:39 +0545

    feat: left top section

--- src/components/molecules/FeedContainer/Left/index.tsx
+++ src/components/molecules/FeedContainer/Left/index.tsx
@@ -1,32 +1,62 @@
-import styled from "styled-components"
+import { Avatar, Divider } from 'antd';
+import { FeedLeftContainerWrapper } from "./index.styled";
 
 
-const FeedLeftContainerWrapper = styled.div`
-        height: 150vh;
-        width: 17rem;
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-        .left__profile__top{
-            border:1px solid blue;
-            height: 35vh;
-            width: 100%;
-        }
-        .left__recent__bottom{
-            border:1px solid green;
-            height: 50vh;
-            width: 100%;
-            position: sticky;
-            top: 5rem;
-        }
-
-`
 
 const FeedLeftContainer = () => {
     return (
         <FeedLeftContainerWrapper>
             <div className="left__profile__top">
+                <div className="cover__image">
+                </div>
+                <div className="profile">
+                    <Avatar shape="circle"
+                        size="large"
+                        style={{
+                            border: "2px solid #fefeff",
+                            width: "5rem",
+                            height: "5rem",
+                        }}
+                        src={"https://plus.unsplash.com/premium_photo-1677220229525-a3107cb60ad5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"}
+                    />
+                    <div className="profile__basic">
+                        <h4
+                            style={{
+                                fontWeight: "600",
+                                fontSize: "16px",
+                                color: " rgba(0, 0, 0, 0.9)",
+                            }}
+                        >Ashish Dhakal</h4>
+                        <p
+                            style={{
+                                fontWeight: "400",
+                                fontSize: "12px",
+                                color: " rgba(0, 0, 0, 0.6)",
+                            }}
+                        >Software Engineer | Go | Python | Django |</p>
+                    </div>
+                    <Divider />
+                    <div className="pv">
 
+                        <div
+                            className="profile__view"
+                        >
+                            <h6>
+                                Who's viewed you profile
+                            </h6>
+                            <h6 className='action'>53</h6>
+                        </div>
+                        <div
+                            className="profile__view"
+                        >
+                            <h6>
+                                Impressions of your post
+                            </h6>
+                            <h6 className='action'>581</h6>
+                        </div>
+                    </div>
+                    <Divider />
+                </div>
 
             </div>
             <div className="left__recent__bottom">
