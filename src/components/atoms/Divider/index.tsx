ec926d4411077a0788dd4df10aeb5bae734ce8a4
Ashish <dhakalashish18@gmail.com>
2023-04-26 16:00:40 +0545

    feat: custom divider component

---
+++ src/components/atoms/Divider/index.tsx
@@ -0,0 +1,11 @@
+import { Divider, DividerProps } from 'antd';
+
+const CustomDivider: React.FC<DividerProps> = () => {
+    return (
+        <Divider
+            style={{ margin: "9px 0 !important" }} />
+
+    )
+}
+
+export default CustomDivider
